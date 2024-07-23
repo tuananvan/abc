5a43c7e3109d21dd19f45d492cfbe98240d685a4
tuananvan <80084018+tuananvan@users.noreply.github.com>
2024-07-23 20:09:21 +0700

    Add files via upload

---
+++ Tong2so.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Nhap a: ";
+	cin >> a ;
+	cout << "Nhap b: ";
+	cin >> b;
+	int c = a + b;
+	cout << "Tong la: " << c;
+	return 0;
+}
